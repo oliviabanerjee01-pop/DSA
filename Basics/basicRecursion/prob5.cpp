70014bed5cb7cde75c280e79afc367e70f5a9c09
oliviabanerjee01 pop <oliviabanerjee01@gmail.com>
2026-06-16 20:04:07 +0530

    Factorial

---
+++ Basics/basicRecursion/prob5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int Factorial(int n){
+    if (n==1){
+        return 1;
+    }
+
+    return n * Factorial(n-1);
+}
+
+int main (){
+     int n;
+    std::cout<<"Input your number: ";
+    std::cin>>n;
+    std::cout<< Factorial(n);
+
+
+}
