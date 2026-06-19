7fa7fd6b034c0cdc0af8522a1748d91b5007a5ef
oliviabanerjee01 pop <oliviabanerjee01@gmail.com>
2026-06-19 15:05:31 +0530

    Counting repeated occurences in an array

---
+++ Basics/basicHashing/prob1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 5, 10, 15, 10, 5};
+    int n = 6;
+    bool visited[n];
+    for(int i = 0; i < n; i++) visited[i] = false;
+
+    for(int i = 0; i < n; i++) {
+        if(visited[i]) continue;
+        int count = 1;
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] == arr[i]) {
+                count++;
+                visited[j] = true;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
